6d75a971bb0a9e9378a7c546f80fc78773022564
Artem Tyurin <spacevillain@ya.ru>
2014-02-18 01:21:13 +0400

    Use LRU-cache

--- routes/post.js
+++ routes/post.js
@@ -1,17 +1,34 @@
 var LiveJournal = require('livejournal');
 
-exports.get = function(req, res) {
+var LRU = require('lru-cache');
 
-  console.log('get');
+var cache = LRU({
+  max: 500,
+  maxAge: 1000 * 60 * 10
+});
+
+exports.get = function(req, res) {
+  var key = req.query.user + '-' + req.query.post_id;
+
+  if (cache.has(key)) {
+    console.log('cached post');
+    return res.json(cache.get(key));
+  }
+
   LiveJournal.RPC.getevents({
     journal: req.query.user,
-    auth_method: 'noauth',
-    selecttype: 'one',
     ditemid: req.query.post_id,
+
+    auth_method: 'noauth',
+    selecttype:  'one',
+
     get_video_ids: true
-  }, function(err, post) {
-    console.log(arguments);
-    res.json(post.events[0]);
+  }, function(err, rpc) {
+    var post = rpc.events[0];
+
+    cache.set(key, post);
+
+    res.json(post);
   });
 
 };
