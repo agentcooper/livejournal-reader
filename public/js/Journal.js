d7575c651f370f8ca411924f0d8624765a6e18cb
Artem Tyurin <artem.tyurin@gmail.com>
2015-05-26 23:28:57 +0200

    Use key for JournalEntry

--- public/js/Journal.js
+++ public/js/Journal.js
@@ -7,6 +7,33 @@ var { Route, DefaultRoute, RouteHandler, Link } = Router;
 
 var LJ = require('./LJ');
 
+var JournalEntry = React.createClass({
+  render: function() {
+    var entry = this.props.entry;
+
+    return (
+      <li className="b-entry">
+        <span className="b-boop__header">
+          <Link to="post" params={
+            {
+              journal: entry.journal || '',
+              postId: entry.postId || ''
+            }
+            }>
+            { entry.subject || '(no subject)' }
+          </Link>
+        </span>
+
+        <div className="b-entry__body">{ entry.body }</div>
+
+        <p className="b-entry__tags">{ entry.props.tags }</p>
+
+        <span className="b-boop__comments">{ entry.reply_count }</span>
+      </li>
+    );
+  }
+});
+
 var Journal = React.createClass({
   contextTypes: {
     router: React.PropTypes.func
@@ -33,31 +60,7 @@ var Journal = React.createClass({
       <ol className="b-feed">
         {
           this.state.journal.events && this.state.journal.events.map((entry) => {
-            return (
-              <li className="b-entry">
-                <span className="b-boop__header">
-                  <Link to="post" params={
-                    {
-                      journal: entry.journal || '',
-                      postId: entry.postId || ''
-                    }
-                    }>
-                    { entry.subject || '(no subject)' }
-                  </Link>
-                </span>
-
-                <div className="b-post_aside">
-                  <img className="b-post__userpic" src="{entry.poster_userpic_url}" />
-                  <span className="b-post__username">{entry.username}></span>
-                </div>
-
-                <div className="b-entry__body">{ entry.body }</div>
-
-                <p className="b-entry__tags">{ entry.props.tags }</p>
-
-                <span className="b-boop__comments">{ entry.reply_count }</span>
-              </li>
-            )
+            return <JournalEntry entry={entry} key={entry.postId} />
           })
         }
       </ol>
