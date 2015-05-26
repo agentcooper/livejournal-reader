783c00f8b4f8d5106aff522c215e1859993044b7
Artem Tyurin <artem.tyurin@gmail.com>
2015-05-27 00:23:02 +0200

    Enable html5 routing

--- public/js/index.js
+++ public/js/index.js
@@ -54,6 +54,6 @@ var routes = (
   </Route>
 );
 
-Router.run(routes, function (Handler) {
+Router.run(routes, Router.HistoryLocation, function (Handler) {
   React.render(<Handler/>, document.body);
 });
