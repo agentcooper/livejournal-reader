510ad0a2de103ae98598609852b887dcd5d7c21f
Artem Tyurin <artem.tyurin@gmail.com>
2015-06-01 22:16:57 +0200

    Correct render of deleted comments. Closes #19

--- public/js/Comment.js
+++ public/js/Comment.js
@@ -19,17 +19,30 @@ var Comment = React.createClass({
   render: function() {
     var comment = this.props.comment;
 
+    var isDeleted = comment.state === 'D';
+
     var commentBox;
 
     if (this.props.commentBox) {
       commentBox = <CommentBox onSubmit={this.props.onSubmit}/>
     }
 
+    var journal = comment.identity_display || comment.postername;
+    var body = comment.body;
+
+    if (isDeleted) {
+      journal = 'deleted';
+      body = 'deleted comment';
+    }
+
     return (
       <li className={classNames(
           'b-comment',
           'b-comment-level-' + (comment.level > 15 ? 'over-15' : comment.level),
-          { 'b-comment--anonymous': comment.isAnonymous }
+          {
+            'b-comment--anonymous': comment.isAnonymous,
+            'b-comment--deleted': isDeleted
+          }
         )} id={ 'c' + comment.dtalkid }>
         <div className={classNames(
           'b-thread__comment',
@@ -42,17 +55,21 @@ var Comment = React.createClass({
           <Link className="b-thread__username" to="journal" params={
             { journal: comment.identity_display || comment.postername || '' }
           }>
-            { comment.identity_display || comment.postername }
+            { journal }
           </Link>
 
-          <a
-            href="javascript:void(0);"
-            onClick={this.props.onReply.bind(null, comment)}
-            className="b-reply b-pseudo">
-            reply
-            </a>
+          {
+            !isDeleted ?
+              <a
+                href="javascript:void(0);"
+                onClick={this.props.onReply.bind(null, comment)}
+                className="b-reply b-pseudo">
+                reply
+                </a>
+              : null
+          }
 
-          <p className="b-thread__body">{ comment.body }</p>
+          <p className="b-thread__body">{ body }</p>
         </div>
 
         {commentBox}
