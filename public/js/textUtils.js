86e20bbfd44fc6432315941991a1231ee71b1cca
Dmitry Petrov <dpetroff@gmail.com>
2015-06-02 09:52:34 +0200

    add url matching to stringUtils. Highlighter should take care not to transform links, that are already inside of some tags

--- public/js/textUtils.js
+++ public/js/textUtils.js
@@ -15,6 +15,9 @@ module.exports = (function() {
   var rx_lj_tag_start = /<lj[^>]*>/g;
   var rx_lj_tag_end = /<\/lj>/g;
 
+  // taken from https://mathiasbynens.be/demo/url-regex solution by @diegoperini
+  var rx_url = /(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9]+-?)*[a-z0-9]+)(?:\.(?:[a-z0-9]+-?)*[a-z0-9]+)*(?:\.(?:[a-z]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig
+
   function process(body) {
     body = body.replace('<a name="Read more..."></a>', '');
 
@@ -127,6 +130,13 @@ module.exports = (function() {
     }
 
     return process( p(text) );
+  }
+
+  factory.hightlightUrls = function(text) {
+    return text
+            .replace(rx_url, function(match) {
+              return "<a href=\"" + match + "\">" + decodeURIComponent(match) + "</a>";
+            });
   }
 
   // auto-paragraph
