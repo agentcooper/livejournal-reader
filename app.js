b35245e7c4907aa9396fd21574ac519716b46c29
Artem Tyurin <artem.tyurin@gmail.com>
2015-08-27 22:59:54 +0200

    Upgrade to Express 4

--- app.js
+++ app.js
@@ -1,87 +1,59 @@
 'use strict';
 
 var express = require('express'),
-    http    = require('http'),
-    path    = require('path'),
+    path = require('path');
 
-    comments = require('./routes/comments'),
-    post     = require('./routes/post'),
-    feed     = require('./routes/feed'),
-    journal  = require('./routes/journal'),
-    update   = require('./routes/update'),
+var comments = require('./routes/comments'),
+    post = require('./routes/post'),
+    feed = require('./routes/feed'),
+    journal = require('./routes/journal'),
+    update = require('./routes/update'),
+    auth = require('./routes/auth');
 
-    auth     = require('./routes/auth');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var serveStatic = require('serve-static');
+var serveFavicon = require('serve-favicon');
+var morgan = require('morgan');
 
 var app = express();
 
-app.use(express.cookieParser());
+app.use(cookieParser());
 
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.set('port', process.env.PORT || 4000);
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use(express.logger('dev'));
-app.use(app.router);
+app.use(serveFavicon(__dirname + '/public/images/favicon.ico'));
+app.use(morgan('combined'));
 
-app.use(function(req, res, next) {
+app.use(serveStatic(path.join(__dirname, 'public')));
 
-  if (/^\/read\//.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/update\//.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/social/.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/feed\/?$/.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/login\/?$/.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/history\/?$/.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/comments$/.test(req.url)) {
-    req.url = '/';
-  }
-
-  if (/^\/about$/.test(req.url)) {
-    req.url = '/';
-  }
-
-  next();
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
 if (app.get('env') === 'development') {
-  app.use(express.errorHandler());
+  app.use(require('errorhandler')());
 }
 
-app.get( '/api/post',     post.get     );
-app.get( '/api/comments', comments.get );
-app.get( '/api/feed',     feed.get     );
-app.get( '/api/journal',  journal.get  );
+app.get('/api/post', post.get);
+app.get('/api/comments', comments.get);
+app.get('/api/feed', feed.get);
+app.get('/api/journal', journal.get);
 
-app.get( '/auth/run',   auth.run   );
-app.get( '/auth',      auth.token );
+app.get('/auth/run', auth.run);
+app.get('/auth', auth.token);
+app.get('/api/login', auth.login);
 
-app.get( '/api/login', auth.login );
-
-app.post( '/api/comments/add', comments.add );
-
+app.post('/api/comments/add', comments.add);
 app.post('/api/newPost', update.newPost);
 app.post('/api/editPost', update.editPost);
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('App is running on port ' + app.get('port'));
+app.all('*', function(req, res) {
+  res.sendfile('./public/index.html');
+});
+
+app.set('port', process.env.PORT || 4000);
+
+var server = app.listen(app.get('port'), function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('livejournal-reader is listening at port %s', port);
 });
